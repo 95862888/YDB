af79751cd49ccd38edf3ac5237912f907d562982
95862888 <71820716+95862888@users.noreply.github.com>
2024-04-17 21:46:31 +0300

    Add files via upload

---
+++ ydbCRUD/Form1.cs
@@ -0,0 +1,530 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+using Ydb.Sdk;
+using Ydb.Sdk.Auth;
+using Ydb.Sdk.Services.Table;
+using Ydb.Sdk.Value;
+
+namespace OnlineStore
+{
+    public partial class Form1 : Form
+    {
+
+        private string id = "";
+        private int intRow = 0;
+        private string nameTable = "Отделы";
+        private string connectNameTable = "Department";
+        private Driver driver;
+        private DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetMe();
+            InitDB();
+
+        }
+
+        private async Task InitDB()
+        {
+            var endpoint = "grpcs://ydb.serverless.yandexcloud.net:2135";
+            var database = "/ru-central1/b1gdr3lhj3btpt3e9nm7/etnc26pvr2hpkndq8lkv";
+            var token = "t1.9euelZqWkJCNnMvGl5qPioyQlsbMjO3rnpWayJSSi5SZzJmYnsuPmJuKj8vl9PcWOh9Q-e8MJCu_3fT3VmgcUPnvDCQrv83n9euelZrJi5THmpOZyM2Li8yRmMbInO_8xeuelZrJi5THmpOZyM2Li8yRmMbInA.rhvvhln8UvXOyVoCipu8IvbTy-CXec4srj-NEiXqRepB8pZZPP1rarV0NOsdSdJG1k24g_FWdbHIhu72Bz_yAA";
+            var config = new DriverConfig(
+                endpoint: endpoint,
+                database: database,
+                credentials: new TokenProvider(token)
+            );
+
+            driver = new Driver(
+                config: config
+            );
+
+            await driver.Initialize();
+            loadData();
+        }
+
+        private void resetMe()
+        {
+            id = string.Empty;
+
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+
+            nameTextBox.Text = "";
+
+
+            customerUpdateButton.Text = "Изменить ()";
+            customerDeleteButton.Text = "Удалить ()";
+            productUpdateButton.Text = "Изменить ()";
+            productDeleteButton.Text = "Удалить ()";
+        }
+
+        private async Task loadData()
+        {
+
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            switch (nameTable)
+            {
+                case "Отделы":
+                    connectNameTable = "Department";
+                    break;
+                case "Статьи затрат":
+                    connectNameTable = "Products";
+                    break;
+                case "Отчеты о затратах":
+                    connectNameTable = "Orders";
+                    break;
+                default:
+                    break;
+            }
+
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @$"SELECT * FROM {connectNameTable}";
+
+                return await session.ExecuteDataQuery(
+                query: query,
+                txControl: TxControl.BeginSerializableRW().Commit()
+                );
+            });
+            response.Status.EnsureSuccess();
+            var queryResponse = (ExecuteDataQueryResponse)response;
+            var resultSet = queryResponse.Result.ResultSets[0];
+
+            dt = new DataTable();
+
+            switch (nameTable)
+            {
+                case "Отделы":
+                    dt.Columns.Add("Id", typeof(ulong));
+                    dt.Columns.Add("Отдел", typeof(string));
+                    dt.Columns.Add("Количество сотрудников", typeof(string));
+                    foreach (var row in resultSet.Rows)
+                    {
+                        dt.Rows.Add((ulong?)row["Id"], (string?)row["Name"],
+                            (ulong?)row["NumberOfEmployees"]);
+                    }
+                    break;
+                case "Статьи затрат":
+                    dt.Columns.Add("Id", typeof(ulong));
+                    dt.Columns.Add("Статья затрат", typeof(string));
+                    dt.Columns.Add("План затрат", typeof(ulong));
+                    foreach (var row in resultSet.Rows)
+                    {
+                        dt.Rows.Add((ulong?)row["Id"], (string?)row["Name"], (ulong?)row["Plan"]);
+                    }
+                    break;
+                case "Отчеты о затратах":
+                    dt.Columns.Add("Id", typeof(ulong));
+                    dt.Columns.Add("Id статьи затрат", typeof(ulong));
+                    dt.Columns.Add("Описание", typeof(string));
+                    dt.Columns.Add("Потраченная сумма", typeof(ulong));
+                    foreach (var row in resultSet.Rows)
+                    {
+                        dt.Rows.Add((ulong?)row["Id"], (ulong?)row["ZatratId"], (string?)row["Name"],
+                            (ulong?)row["Amount"]);
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                intRow = Convert.ToInt32(dt.Rows.Count.ToString());
+            }
+            else
+            {
+                intRow = 0;
+            }
+
+            toolStripStatusLabel1.Text = "Number of row(s): " + intRow.ToString();
+
+            DataGridView dgv1 = dataGridView1;
+
+            dgv1.MultiSelect = false;
+            dgv1.AutoGenerateColumns = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dgv1.DataSource = dt;
+
+            dgv1.Columns[0].Width = 55;
+
+            dgv1.Columns[1].Width = 250;
+
+            dgv1.Columns[2].Width = 250;
+        }
+
+        private ulong GetLastIdFromTable()
+        {
+            ulong maxId = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if ((ulong)dt.Rows[i]["Id"] > maxId)
+                    maxId = (ulong)dt.Rows[i]["Id"];
+            }
+            return maxId + 1;
+        }
+
+        private async void customerInsertButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(firstNameTextBox.Text.Trim())
+                || string.IsNullOrEmpty(lastNameTextBox.Text.Trim()))
+             
+            {
+                MessageBox.Show("Пожалуйста введите информацию об отделе полностью.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @"
+                    DECLARE $Id AS Uint64;
+                    DECLARE $Name AS Utf8;
+                    DECLARE $NumberOfEmployees AS Uint64;
+          
+
+                    UPSERT INTO Customers (Id, Name, NumberOfEmployees) VALUES
+                        ($Id, $Name, $NumberOfEmployees);
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                        {
+                { "$Id", YdbValue.MakeUint64(GetLastIdFromTable()) },
+                { "$Name", YdbValue.MakeUtf8(firstNameTextBox.Text.Trim()) },
+                { "$NumberOfEmployees", YdbValue.MakeUtf8(lastNameTextBox.Text.Trim()) },
+         
+                        }
+                );
+            });
+
+            response.Status.EnsureSuccess();
+            if (response.Status.StatusCode == StatusCode.Success)
+                MessageBox.Show("Данные сохранены.", "Успех",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            loadData();
+            resetMe();
+        }
+
+        private async void customerUpdateButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.id))
+            {
+                MessageBox.Show("Пожалуйста выберите элемент из списка.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(lastNameTextBox.Text.Trim())
+                || string.IsNullOrEmpty(firstNameTextBox.Text.Trim()))
+            {
+                MessageBox.Show("Пожалуйста введите данные об отделе полностью.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @"
+                    DECLARE $Id AS Uint64;
+                    DECLARE $Name AS Utf8;
+                    DECLARE $NumberOfEmployees AS Uint64;
+          
+
+                    UPSERT INTO Customers (Id, Name, NumberOfEmployees) VALUES
+                        ($Id, $Name, $NumberOfEmployees);
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                        {
+                { "$Id", YdbValue.MakeUint64(ulong.Parse(this.id)) },
+               { "$Name", YdbValue.MakeUtf8(firstNameTextBox.Text.Trim()) },
+                { "$NumberOfEmployees", YdbValue.MakeUtf8(lastNameTextBox.Text.Trim()) },
+
+                        }
+                );
+            });
+
+            response.Status.EnsureSuccess();
+            if (response.Status.StatusCode == StatusCode.Success)
+                MessageBox.Show("Данные успешно сохранены.", "Успех",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            loadData();
+            resetMe();
+        }
+
+        private async void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.id))
+            {
+                MessageBox.Show("Пожалуйста выберите элемент из списка.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (MessageBox.Show("Вы уверены что хотите удалить данный элемент?", "Удаление данных",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                var tableClient = new TableClient(driver, new TableClientConfig());
+
+
+                var response = await tableClient.SessionExec(async session =>
+                {
+                    var query = @$"
+                        DECLARE $Id AS Uint64;        
+
+                        DELETE FROM {connectNameTable} WHERE Id == $Id;
+                    ";
+
+                    return await session.ExecuteDataQuery(
+                        query: query,
+                        txControl: TxControl.BeginSerializableRW().Commit(),
+                        parameters: new Dictionary<string, YdbValue>
+                            {
+                { "$Id", YdbValue.MakeUint64(ulong.Parse(this.id)) }
+                            }
+                    );
+                });
+                response.Status.EnsureSuccess();
+                if (response.Status.StatusCode == StatusCode.Success)
+                    MessageBox.Show("Элемент был удален.", "Успех",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                loadData();
+                resetMe();
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex != -1)
+            {
+                DataGridView dgv1 = dataGridView1;
+
+                this.id = Convert.ToString(dgv1.CurrentRow.Cells[0].Value);
+                customerUpdateButton.Text = "Изменить (" + this.id + ")";
+                customerDeleteButton.Text = "Удалить (" + this.id + ")";
+                productUpdateButton.Text = "Изменить (" + this.id + ")";
+                productDeleteButton.Text = "Удалить (" + this.id + ")";
+            }
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nameTable = tabControl1.TabPages[tabControl1.SelectedIndex].Text;
+            loadData();
+            resetMe();
+        }
+
+        private async void productInsertButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nameTextBox.Text.Trim()))
+            {
+                MessageBox.Show("Пожалуйста введите название статьи затрат", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @"
+                    DECLARE $Id AS Uint64;
+                    DECLARE $Name AS Utf8;
+                    DECLARE $Plan AS Uint64;
+
+                    UPSERT INTO Products (Id, Name,Plan) VALUES
+                        ($Id, $Name,$Plan);
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                        {
+                { "$Id", YdbValue.MakeUint64(GetLastIdFromTable()) },
+                { "$Name", YdbValue.MakeUtf8(nameTextBox.Text.Trim()) },
+                 {"$Plan",YdbValue.MakeUint64(Convert.ToUInt64(priceTextBox.Text.Trim())) },
+
+                        }
+                );
+            });
+            response.Status.EnsureSuccess();
+            if (response.Status.StatusCode == StatusCode.Success)
+                MessageBox.Show("Данные сохранены", "Успех",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            loadData();
+            resetMe();
+        }
+
+        private async void productUpdateButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.id))
+            {
+                MessageBox.Show("Пожалуйста выберите элемент из списка", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(nameTextBox.Text.Trim()))
+            {
+                MessageBox.Show("Пожалуйста введите название.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @"
+                    DECLARE $Id AS Uint64;
+                    DECLARE $Name AS Utf8;
+                    DECLARE $Plan AS Uint64;
+
+                    UPSERT INTO Products (Id, Name,Plan) VALUES
+                        ($Id, $Name, $Plan);
+                ";
+
+                return await session.ExecuteDataQuery(
+                    query: query,
+                    txControl: TxControl.BeginSerializableRW().Commit(),
+                    parameters: new Dictionary<string, YdbValue>
+                        {
+                { "$Id", YdbValue.MakeUint64(ulong.Parse(this.id)) },
+                { "$Name", YdbValue.MakeUtf8(nameTextBox.Text.Trim()) },
+                 {"$Plan",YdbValue.MakeUint64(Convert.ToUInt64(priceTextBox.Text.Trim())) },
+                        }
+                );
+            });
+            response.Status.EnsureSuccess();
+            if (response.Status.StatusCode == StatusCode.Success)
+                MessageBox.Show("Данные сохранены.", "Успех",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            loadData();
+            resetMe();
+        }
+
+        private async void ReportWorker_Click(object sender, EventArgs e)
+        {
+            dt.Clear();
+            dt = new DataTable();
+            var tableClient = new TableClient(driver, new TableClientConfig());
+     
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @$"SELECT * FROM CustomersReport";
+
+                return await session.ExecuteDataQuery(
+                query: query,
+                txControl: TxControl.BeginSerializableRW().Commit()
+                );
+            });
+            response.Status.EnsureSuccess();
+            var queryResponse = (ExecuteDataQueryResponse)response;
+            var resultSet = queryResponse.Result.ResultSets[0];
+
+
+
+            dt.Columns.Add("Id", typeof(ulong));
+            dt.Columns.Add("Название отдела", typeof(string));
+            dt.Columns.Add("План", typeof(string));
+            dt.Columns.Add("Затраченная сумма", typeof(string));
+            dt.Columns.Add("Соотношение затрачено/план", typeof(float));
+
+            foreach (var row in resultSet.Rows)
+            {
+                dt.Rows.Add((ulong?)row["Id"], (string?)row["DepatmentName"],
+            
+                    (ulong?)row["Plan"], (ulong?)row["Amount"], (float?)(Convert.ToDouble((ulong?)row["Amount"]) / Convert.ToDouble((ulong?)row["Plan"])));
+            }
+
+            DataGridView dgv1 = dataGridView1;
+
+            dgv1.MultiSelect = false;
+            dgv1.AutoGenerateColumns = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dgv1.DataSource = dt;
+
+            dgv1.Columns[0].Width = 55;
+        }
+
+        private async void ReportProduct_Click(object sender, EventArgs e)
+        {
+            dt.Clear();
+            dt = new DataTable();
+            var tableClient = new TableClient(driver, new TableClientConfig());
+       
+            var response = await tableClient.SessionExec(async session =>
+            {
+                var query = @$"SELECT * FROM ProductsReport";
+
+                return await session.ExecuteDataQuery(
+                query: query,
+                txControl: TxControl.BeginSerializableRW().Commit()
+                );
+            });
+            response.Status.EnsureSuccess();
+            var queryResponse = (ExecuteDataQueryResponse)response;
+            var resultSet = queryResponse.Result.ResultSets[0];
+
+
+
+            dt.Columns.Add("Id", typeof(ulong));
+            dt.Columns.Add("Статья затрат", typeof(string));
+            dt.Columns.Add("Затрачено", typeof(ulong));
+            foreach (var row in resultSet.Rows)
+            {
+                dt.Rows.Add((ulong?)row["Id"], (string?)row["ProductName"], (ulong?)row["Amount"]);
+            }
+
+            DataGridView dgv1 = dataGridView1;
+
+            dgv1.MultiSelect = false;
+            dgv1.AutoGenerateColumns = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dgv1.DataSource = dt;
+
+            dgv1.Columns[0].Width = 55;
+        }
+    }
+}
